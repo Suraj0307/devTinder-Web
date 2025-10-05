ab932e527fc151402bc92a440eab9e34fec1fc32
Suraj joshi <surajjoshi@192.168.101.3>
2025-10-05 16:40:44 +0530

    Season 2 Episode 16

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import Body from "./components/Body";
 import Login from "./components/Login";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import appStore from "./utils/appStore.js";
+import { Provider } from "react-redux";
+import Feed from "./components/Feed.jsx";
 
 function App() {
   return (
-    <BrowserRouter basename="/">
-    <Routes>
-      <Route path="/" element={<Body/>}>
-        <Route path="/login" element={<Login/>}/>
+    <Provider store={appStore} >
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/" element={<Feed />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ Provider>
 
-      </Route>
-    </Routes>
-    </BrowserRouter>
   );
 }
 
